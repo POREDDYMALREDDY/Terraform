46feefd6b7f8eb3f065fe29f57cb24f867799258
POREDDYMALREDDY <108260081+POREDDYMALREDDY@users.noreply.github.com>
2022-12-16 21:12:41 +0530

    t6

--- main.tf
+++ main.tf
@@ -2,16 +2,12 @@ provider "aws" {
 region = "us-east-1"
 }
 resource "aws_instance" "one" {
+count = var.instance_count
 ami = "ami-0b0dcb5067f052a63"
 instance_type = "t2.medium"
 tags = {
 Name = "Terraform_instance"
 }
 }
-resource "aws_ebs_volume" "two" {
-availability_zone = "us-east-1a"
-size = 50
-tags = {
-Name = "Hello Devops"
-}
-}
+
+
