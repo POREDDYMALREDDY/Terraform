7c495d6dbf064f36cd2e74a522ad65a6a30b6e1a
POREDDYMALREDDY <108260081+POREDDYMALREDDY@users.noreply.github.com>
2022-12-16 21:23:10 +0530

    t12

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
+
 provider "aws" {
 region = "us-east-1"
+access_key = "AKIAXNVEXVGNFBLJLL3L"
+secret_key = "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"
 }
 resource "aws_instance" "one" {
 ami = "ami-0b0dcb5067f052a63"
